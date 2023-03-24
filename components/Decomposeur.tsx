19a3763c1519fe56ae66d7db164814df91ba3211
esHome33 <etienschwa@gmail.com>
2023-03-24 16:57:52 +0100

    V2 affichage erreurs plus court

--- components/Decomposeur.tsx
+++ components/Decomposeur.tsx
@@ -96,7 +96,7 @@ const Decomposeur = () => {
 			})
 			.catch((err) => {
 				if (axios.isAxiosError(err)) {
-					setdec1([JSON.stringify(err)]);
+					setdec1([err.name , err.message]);
 					setdec2(["Erreur AXIOS"]);
 				} else {
 					setdec1(["Erreur au niveau du serveur !"]);
